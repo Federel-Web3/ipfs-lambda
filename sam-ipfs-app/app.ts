47f5e3bb8aa0e08d5d4f6a8e0cf916e5a69a3fd5
pedro bufulin <pedro.almeida@ufu.br>
2022-12-05 02:39:35 -0300

    first commit

---
+++ sam-ipfs-app/app.ts
@@ -0,0 +1,43 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ *
+ */
+
+interface GoodsOrRealEstate {
+    name: string;
+    description: string;
+}
+
+export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    let response: APIGatewayProxyResult;
+    try {
+        if (!event.body) throw Error();
+        const goodOrRealEstate: GoodsOrRealEstate = JSON.parse(event.body);
+        console.log(JSON.stringify(event));
+
+        response = {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: goodOrRealEstate.description,
+                env: process.env,
+            }),
+        };
+    } catch (err: unknown) {
+        console.error(err);
+        response = {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: err instanceof Error ? err.message : 'some error happened',
+            }),
+        };
+    }
+
+    return response;
+};
